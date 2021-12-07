fef4afb696988d5c538d0ef26902ced069b5117d
StevoMc <stevenmc59@gmail.com>
2021-12-07 13:12:06 +0100

    Macros

--- src/main.c
+++ src/main.c
@@ -1,6 +1,33 @@
-#define F_CPU 16000000UL
 #include <avr/io.h>
-#include <avr/delay.h>
+#include <util/delay.h>
+#define F_CPU 16000000UL
+
+/*
+ ?   GET_BIT
+ *  -> Macro for retrieving bit from given position
+ */
+#define GET_BIT(x, pos) (((x) & (1 << pos)) >> pos)
+
+/*
+ ?  SET_BIT
+ *  -> Macro for setting bit in given position to 1 by generating a mask 
+ *     in the proper bit location and ORing (|) x with the mask.
+ */
+#define SET_BIT(x, pos) (x |= (1U << pos))
+
+/* 
+ ?   TOOGLE_BIT
+ *  -> Macro for toggling bit in given position to 1 by generating a mask 
+ *     in the proper bit location and ex-ORing x with the mask. 
+ */
+#define TOOGLE_BIT(x, pos) (x ^= (1U << pos))
+
+/* 
+ ?  CLEAR_BIT
+ *  -> Macro for clearing bit in given position to 0 by generating a mask
+ *     in the proper bit location and Anding x with the mask.
+ */
+#define CLEAR_BIT(x, pos) (x &= (~(1U << pos)))
 
 int Ampel(void);
 int Input(void);
@@ -8,8 +35,9 @@ int delay = 250;
 
 int main(void)
 {
+    lrint(GET_BIT(0b01, 0));
 
-    Input();
+    // Input();
     // Ampel();
 }
 
@@ -17,13 +45,18 @@ int Ampel(void)
 {
     DDRB |= (1 << DDB2) | (1 << DDB3) | (1 << DDB4);
 
+    /* Clear Bits */
+    CLEAR_BIT(PORTB, PORTB2);
+    CLEAR_BIT(PORTB, PORTB3);
+    CLEAR_BIT(PORTB, PORTB4);
+
     /* RESTET OUTPUT TO 0 */
-    PORTB &= ~(1 << PORTB2);
-    PORTB &= ~(1 << PORTB3);
-    PORTB &= ~(1 << PORTB4);
+    // PORTB &= ~(1 << PORTB2);
+    // PORTB &= ~(1 << PORTB3);
+    // PORTB &= ~(1 << PORTB4);
+
     while (1)
     {
-
         // red
         PORTB |= (1 << PORTB2);
         _delay_ms(delay * 4);
